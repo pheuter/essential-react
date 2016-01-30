4eaf4fbedf6e335dd9ba569fefd8c35e62d17fb2
Dylan <thatguy72@gmail.com>
2016-01-31 00:31:28 +0800

    checked for all errors

--- src/common/components/App.js
+++ src/common/components/App.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 
 export default class App extends React.Component {
+  static propTypes = {
+    children: React.PropTypes.element.isRequired,
+  }
   render() {
     return (
-      <div id='container'>
+      <div id="container">
         {this.props.children}
       </div>
     );
