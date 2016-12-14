2a4bf77c840ac9587076d629122d49f4d31a982a
Connor Ray <connor.ray.cb@gmail.com>
2016-12-14 12:27:14 -0700

    Sets up skeleton with routes

--- src/common/components/Routes.js
+++ src/common/components/Routes.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import App from './App';
-import LoginPage from '../../pages/login/page';
 import HomePage from '../../pages/home/page';
+import AboutPage from '../../pages/about/page';
+import ContactPage from '../../pages/contact/page';
 
 
 export default (
   <Route path="/" component={App}>
-    <IndexRoute component={LoginPage} />
-    <Route path="home" component={HomePage} />
+    <IndexRoute component={HomePage} />
+    <Route path="about" component={AboutPage} />
+    <Route path="contact" component={ContactPage} />
   </Route>
 );
